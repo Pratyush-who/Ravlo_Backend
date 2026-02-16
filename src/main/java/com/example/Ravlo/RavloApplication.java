263b7346ea6f5929c04ccb1d394a3efd15c48816
pratyush-who <pratyushmehra2005@gmail.com>
2026-02-16 10:25:42 +0530

    first commit

---
+++ src/main/java/com/example/Ravlo/RavloApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ravlo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RavloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RavloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Ravlo/RavloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ravlo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RavloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
